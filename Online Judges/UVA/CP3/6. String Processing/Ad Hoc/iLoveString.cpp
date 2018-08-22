5e792f2996212d2379b6147d4baf090bc0d02e98
Dicky Novanto <dickynovanto1103@gmail.com>
2018-08-22 14:46:26 +0700

    rename variables to english

--- Online Judges/UVA/CP3/6. String Processing/Ad Hoc/iLoveString.cpp
+++ Online Judges/UVA/CP3/6. String Processing/Ad Hoc/iLoveString.cpp
@@ -34,9 +34,9 @@ struct vertex{
 
 vector<vertex> trie;
 
-void addString(string kata, int idx){
+void addString(string word, int idx){
 	int node = 0;
-	for(char kar: kata){
+	for(char kar: word){
 		int bil = kar - 'A';
 		if(trie[node].next[bil] == -1){
 			trie[node].next[bil] = trie.size();
@@ -48,26 +48,26 @@ void addString(string kata, int idx){
 	trie[node].idx.pb(idx);
 }
 
-int jalan(int node, char nextChar);
+int go(int node, char nextChar);
 
 int getLink(int node){
 	if(trie[node].link == -1){
 		if(node == 0 || trie[node].parent == 0){
 			trie[node].link = 0;
 		}else{
-			trie[node].link = jalan(getLink(trie[node].parent), trie[node].kar);
+			trie[node].link = go(getLink(trie[node].parent), trie[node].kar);
 		}
 	}
 	return trie[node].link;
 }
 
-int jalan(int node, char nextChar){
+int go(int node, char nextChar){
 	int bil = nextChar - 'A';
 	if(trie[node].go[bil] == -1){
 		if(trie[node].next[bil] != -1){
 			trie[node].go[bil] = trie[node].next[bil];
 		}else{
-			trie[node].go[bil] = (node == 0 ? 0: jalan(getLink(node), nextChar));
+			trie[node].go[bil] = (node == 0 ? 0: go(getLink(node), nextChar));
 
 		}
 	}
@@ -80,44 +80,44 @@ int main(){
 	FastSlowInput
 	int tc,i,j;
 	cin>>tc;
-	bool ada[maxn];
+	bool isExist[maxn];
 	while(tc--){
-		string kata;
-		string listKata[maxn];
-		cin>>kata;
+		string word;
+		string listOfWord[maxn];
+		cin>>word;
 		int n;
 		cin>>n;
 		trie.assign(1, vertex());
 		for(i=0;i<n;i++){
-			cin>>listKata[i];
-			addString(listKata[i],i);
+			cin>>listOfWord[i];
+			addString(listOfWord[i],i);
 		}
-		memset(ada, false, sizeof ada);
+		memset(isExist, false, sizeof isExist);
 		int node = 0;
 		char lastKar;
-		for(char kar : kata){
+		for(char kar : word){
 			lastKar = kar;
-			node = jalan(node, kar);
+			node = go(node, kar);
 			if(trie[node].leaf){
 				for(int idx: trie[node].idx){
-					ada[idx] = true;
+					isExist[idx] = true;
 				}
 			}
 		}
 		if(node != 0){
-			jalan(node,lastKar);
+			go(node,lastKar);
 			while(node>0){
 				node = getLink(node);
 				if(trie[node].leaf){
 					for(int idx: trie[node].idx){
-						ada[idx] = true;
+						isExist[idx] = true;
 					}
 				}
 			}
 		}
 
 		for(i=0;i<n;i++){
-			if(ada[i]){cout<<"y"<<endl;}
+			if(isExist[i]){cout<<"y"<<endl;}
 			else{cout<<"n"<<endl;}
 		}
 		trie.clear();

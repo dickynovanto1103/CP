c4697e4067517480635cdf360785b615303c11cd
Dicky Novanto <dickynovanto1103@gmail.com>
2019-04-12 14:27:43 +0700

    remove duplicated code

--- Online Judges/Codeforces/Intel Code Final Challenge/c.cpp
+++ Online Judges/Codeforces/Intel Code Final Challenge/c.cpp
@@ -15,8 +15,6 @@ typedef vector<int> vi;
 typedef pair<int,int> ii;
 typedef vector<ii> vii;
 
-int n,m,k;
-
 int gcd(int a, int b, int &x, int &y) {
     if (a == 0) {
         x = 0; y = 1;
@@ -42,22 +40,9 @@ bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
     return true;
 }
 
-ll solve(int a, int b) {
-	a -= b;
-	int koef = 2*m - 2*n;
-	if(a % koef == 0){
-		int x = a/koef;
-		ll ans = (ll)b + (ll)m*(ll)x;
-		if(ans < 0){return inf;}
-		return ans;
-	}else{
-		return inf;
-	}
-}
-
 int main(){
+	int n,m,k,i,j;
 	scanf("%d %d %d",&n,&m,&k);
-	int i,j;
 	for(i=0;i<k;i++){
 		int x,y;
 		scanf("%d %d",&x,&y);
@@ -84,49 +69,21 @@ int main(){
 				int a,b;
 				if(find_any_solution(koef1, -koef2, c,a,b,g)) {
 					// printf("posX: %d posY: %d a: %d b: %d g: %d\n",posX[j],posY[l],a,b,g);
+					int pengali;
 					if(a >= 0 && b >= 0){
-						int pengali = (-a) / (-koef2);
-						ll x = a + (ll)pengali*(-koef2);
-						assert(x >= 0);
-						ll jawab = x*2LL*(ll)n + (ll)posX[j];
-						// printf("jawab: %lld\n",jawab);
-						// ll lcm = (ll)koef1*koef2;
-						// jawab %= lcm;
-						// printf("lcm: %lld jawab jd: %lld\n", lcm, jawab);
-						if(jawab < 0){continue;}
-						ans = min(ans, jawab);		
+						pengali = (-a) / (-koef2);
+								
 					}else if(a <0 && b < 0){
 						int bagi1 = (-a / (-koef2)) + (-a%(-koef2) == 0 ? 0 : -1);
 						int bagi2 = (-b / (-koef1)) + (-b%(-koef1) == 0 ? 0 : -1);
-						int pengali = min(bagi1, bagi2);
-						ll x = a + (ll)pengali * (-koef2);
-						assert(a+pengali*(-koef2) >= 0);
-						assert(b+pengali*(-koef1) >= 0);
-						ll jawab = x*2LL*n + (ll)posX[j];
-						if(jawab < 0){continue;}
-						ans = min(ans, jawab);
-					}else{
-						//dibikin biar a >= 0 && b >= 0
-						if(a < 0){
-							int pengali = (-b)/(-koef1);
-							ll y = b - (ll)pengali*(ll)(koef1);
-							assert(y >=0);
-							ll jawab = y*2LL*(ll)m + (ll)posY[l];
-							if(jawab < 0){continue;}
-							ans = min(ans, jawab);
-						}else if(b < 0){
-							int pengali = (-a) / (-koef2);
-							ll x = a + (ll)pengali*(-koef2);
-							assert(x >= 0);
-							ll jawab = x*2LL*(ll)n + (ll)posX[j];
-							// printf("jawab: %lld\n",jawab);
-							// ll lcm = (ll)koef1*koef2;
-							// jawab %= lcm;
-							// printf("lcm: %lld jawab jd: %lld\n", lcm, jawab);
-							if(jawab < 0){continue;}
-							ans = min(ans, jawab);		
-						}
+						pengali = min(bagi1, bagi2);	
 					}
+
+					ll x = a + (ll)pengali*(-koef2);
+					assert(x >= 0);
+					ll jawab = x*2LL*(ll)n + (ll)posX[j];
+					if(jawab < 0){continue;}
+					ans = min(ans, jawab);
 				}
 			}
 		}

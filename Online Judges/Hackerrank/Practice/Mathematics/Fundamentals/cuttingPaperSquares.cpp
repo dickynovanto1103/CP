b0cef6e3ea97b89960c54ace1ab84ec734e48a9b
Dicky Novanto <dickynovanto1103@gmail.com>
2020-03-10 00:53:57 +0800

    Add cutting paper problem solution

---
+++ Online Judges/Hackerrank/Practice/Mathematics/Fundamentals/cuttingPaperSquares.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define inf 1000000000
+#define unvisited -1
+#define visited 1
+#define eps 1e-9
+#define mp make_pair
+#define pb push_back
+#define pi acos(-1.0)
+#define uint64 unsigned long long
+#define FastSlowInput ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define debug if(true)
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+
+int main(){
+	ll n,m;
+	scanf("%lld %lld",&n,&m);
+	ll ans1 = n-1 + n*(m-1);
+	ll ans2 = m-1 + m*(n-1);
+	printf("%lld\n",min(ans1,ans2));
+	return 0;
+};

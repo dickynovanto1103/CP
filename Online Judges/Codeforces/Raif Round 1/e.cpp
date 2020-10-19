63d0477fbcbff84d931deee3071c947befbb7c8f
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-10-19 21:41:47 +0800

    Upsolve Raif Round 1 Problem E

--- Online Judges/Codeforces/Raif Round 1/e.cpp
+++ Online Judges/Codeforces/Raif Round 1/e.cpp
@@ -14,125 +14,59 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int,int> ii;
+typedef pair<ll,ii> iii;
 typedef vector<ii> vii;
 
 int a[100001];
-int cnt[1000001];
-int temp[1000001];
 
-bool can(int mid, int n, int k) {
-	int banyak = n;
-	for(int i=0;i<n;i++){
-		if(a[i] > mid){
-			int bagi = (a[i] + mid - 1) / mid;
-			banyak--;
-			banyak+=bagi;
-		}
+ll calcTime(ll bil, ll bagi) {
+	//7 / 3 -> 3 2 2 -> 3x + 2y = 7 
+	//7 / 4 -> 2 2 2 1 -> 
+	// formula: bil / bagi means that the minNum = bil / bagi, the maxNum = bil + bagi-1 / bagi, numOfMaxNum = bil % bagi, numOfMinNum = bagi - bil % bagi
+	if(bil % bagi == 0){
+		ll minBil = bil / bagi;
+		return minBil*minBil*bagi;
+	}else{
+		ll minBil = bil / bagi;
+		ll maksBil = (bil + bagi - 1) / bagi;
+		ll numOfMaxNum = bil % bagi;
+		ll numOfMinNum = bagi - numOfMaxNum;
+		return minBil*minBil*numOfMinNum + maksBil*maksBil*numOfMaxNum;
 	}
-	return banyak <= k;
-}
-
-ll hit() {
-	ll ans = 0;
-	for(int i=1;i<=1000000;i++){
-		ans += ((ll)cnt[i]*(ll)i*(ll)i);
-	}
-	return ans;
 }
 
 int main(){
 	int n,k,i,j;
 	while(scanf("%d %d",&n,&k) != EOF) {
-		priority_queue<int> pq;
-		int minim = inf, maks = 0;
-		for(i=0;i<=1000000;i++){
-			cnt[i] = 0;
-			temp[i] = 0;
-		}
+		priority_queue<iii> pq;
+		ll ans = 0;
 		for(i=0;i<n;i++){
 			scanf("%d",&a[i]);
-			minim = min(minim, a[i]); maks = max(maks, a[i]);
+			ll timeNow = calcTime(a[i], 1);
+			ans += timeNow;
+			ll timeLater = calcTime(a[i], 2);
+			ll diff = timeNow - timeLater;
+			pq.push(iii(diff, ii(a[i], 1)));
 		}
-		int kiri = 1, kanan = maks, mid, ans;
-		// printf("kiri: %d kanan: %d\n",kiri, kanan);
-		while(kiri <= kanan) {
-			mid = (kiri + kanan)/2;
-			if(can(mid, n, k)){
-				ans = mid;
-				kanan = mid-1;
-			}else{
-				kiri = mid+1;
-			}
-		}
-		printf("ans: %d\n",ans);
+		// printf("ans: %lld\n",ans);
+		while(n < k){
+			iii top = pq.top(); pq.pop();
+			ll diff = top.first;
+			ii pas = top.second;
+			int bil = pas.first, numBagi = pas.second;
+			// printf("diff: %lld\n", diff);
 
-		for(i=0;i<n;i++){
-			if(a[i] >= ans){
-				if(a[i] % ans == 0){
-					int banyak = a[i] / ans;
-					int bil = ans;
-					cnt[bil] += banyak;
-				}else{
-					int banyak = a[i] / ans;
-					int sisa = a[i] % ans;
-					cnt[ans] += banyak;
-					cnt[sisa]++;
-					printf("ans: %d banyak: %d sisa: %d\n",ans, banyak, sisa);
-				}
-			}else{
-				temp[a[i]]++;
-				cnt[a[i]]++;
-			}
-		}
-		int total = 0;
-		for(i=1;i<=maks;i++){
-			total += cnt[i];
-		}
-		// printf("total: %d\n",total);
+			ans -= diff;
+			n++;
+			// printf("ans jd: %lld\n", ans);
 
-		for(i=maks;i>=1;i--){
-			if(total == k){
-				break;
-			}
-			if(cnt[i] >= (i-1)){
-				cnt[i] -= (i-1);
-				cnt[i-1] += i;
-				total++; 
-				i++;
-			}
+			ll timeNow = calcTime(bil, numBagi + 1);
+			ll timeLater = calcTime(bil, numBagi + 2);
+			diff = timeNow - timeLater;
+			// printf("push diff: %lld bil: %d %d\n",diff, bil, numBagi);
+			pq.push(iii(diff, ii(bil, numBagi + 1)));
 		}
-		assert(total == k);
-		//optimize more
-		multiset<int> s;
-		for(i=1;i<=maks;i++){
-			int sisa = cnt[i] - temp[i];
-			for(j=0;j<sisa;j++){
-				s.insert(i);
-				printf("insert %d\n",i);
-			}
-		}
-		// printf("size; %d\n",int(s.size()));
-		if(s.size() == 1){
-			printf("%lld\n",hit());
-			continue;
-		}
-
-		while(true) {
-			assert(s.size() > 1);
-			int kecil = *s.begin(), besar = *s.rbegin();
-			if(kecil == besar){break;}
-			int cntKecil = cnt[kecil], cntBesar = cnt[besar];
-			cnt[kecil]--; cnt[besar]--;
-			cnt[kecil+1]++; cnt[besar-1]++;
-			s.erase(s.begin()); s.erase(--s.end());
-			s.insert(kecil+1); s.insert(besar-1);
-			kecil = *s.begin(), besar = *s.rbegin();
-			if(cnt[besar] == cntBesar){
-				break;
-			}
-		}
-
-		printf("%lld\n",hit());
+		printf("%lld\n",ans);
 	}
 	
 	return 0;

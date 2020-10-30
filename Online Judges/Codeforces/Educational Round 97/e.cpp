006108ef9d258723b12327abe8f40df9ca6e9308
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-10-30 23:30:48 +0800

    Add skeleton code for CF Edu Round 97 Problem E

---
+++ Online Judges/Codeforces/Educational Round 97/e.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define inf 1000000000
+#define unvisited -1
+#define visited 1
+#define eps 1e-9
+#define mp make_pair
+#define pb push_back
+#define pi acos(-1.0)
+#define uint64 unsigned long long
+#define FastSlowInput ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define debug if(true)
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+
+int a[500010], idx[500010];
+
+int main(){
+	int n,k,i,j;
+	while(scanf("%d %d",&n,&k) != EOF){
+		for(i=0;i<n;i++){
+			scanf("%d",&a[i]);
+		}
+
+		for(i=0;i<k;i++){
+			scanf("%d",&idx[i]);
+		}
+		
+	}
+	
+	
+	return 0;
+};
